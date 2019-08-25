b39c647fd77079b3b9ac30146a5810963a83b791
Hamidou Diallo <dio.hamidou@gmail.com>
2019-08-25 21:43:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var nowServing = katzDeliLine =>{
   if(katzDeliLine.length === 0){
     return "There is nobody waiting to be served!"
   }else{
-    return `Currently serving ${katzDeliLine[katzDeliLine.unshift()]}`
+    return `Currently serving ${katzDeliLine.slice(0)}`
   }
   
 }

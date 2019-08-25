8410bb67d956b3376de10d99127dad21235e7282
Hamidou Diallo <dio.hamidou@gmail.com>
2019-08-25 21:08:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var katzDeliLine = [];
 
 var takeANumber = (cur,name) =>{
-  return `Welcome, ${name}. You are number ${cur} in line.`
+  return `Welcome, ${name}. You are number ${cur+1} in line.`
 }
 
